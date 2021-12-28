66a342f7dbbefa66ac5432be05e4497dbc06ba6c
Jakob <jakob1519.ee10@nycu.edu.tw>
2021-12-28 14:48:22 +0800

    Fuzzy System Exam

--- fuzzy_final_exam.m
+++ fuzzy_final_exam.m
@@ -8,13 +8,16 @@ evalfis([5 5], washing_machine_f)
 evalfis([5 5; 10 3; 2 9], washing_machine_f)
 
 figure;
-plotmf(washing_machine_f, "input", 1)
+plotmf(washing_machine_f, "input", 1);
+
 figure;
-plotmf(washing_machine_f, "input", 2)
+plotmf(washing_machine_f, "input", 2);
+
 figure;
-plotmf(washing_machine_f, "output", 1)
+plotmf(washing_machine_f, "output", 1);
+
 figure;
-plotmf(washing_machine_f, "output", 2)
+plotmf(washing_machine_f, "output", 2);
 
 showrule(washing_machine_f)
 
